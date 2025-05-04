6c3ee63b2f5f4d93fef2b48e29d9b63cf697eb05
plthabangr <rakgoropothabang@gmail.com>
2025-05-04 17:00:58 +0200

    Task completed with all solution for friont and backend

--- FrontEnd/js/edit.js
+++ FrontEnd/js/edit.js
@@ -29,17 +29,42 @@ async function fetchProductFromAPI(productId) {
 
 // TODO 10: create a function preFillFormData() to fill product details in edit product form
 function preFillFormData(product) {
-
+    document.getElementById("title").value = product.title
+    document.getElementById("price").value = product.price
+    document.getElementById("description").value = product.description
+    document.getElementById("productCategory").value = product.category
+    document.getElementById("imageURL").value = product.image
+    document.getElementById("rating").value = product.rating.rate
+    document.getElementById("ratingCount").value = product.rating.count
 }
 
 async function updateProduct(productId) {
     // TODO 11: fetch the form values and store them in a product object named 'updatedProduct'
+    const updatedProduct = {
+        title: document.getElementById("title").value,
+        price: parseFloat(document.getElementById("price").value),
+        description: document.getElementById("description").value,
+        category: document.getElementById("productCategory").value,
+        image: document.getElementById("imageURL").value,
+        rating: {
+            rate: parseFloat(document.getElementById("rating").value),
+            count: parseInt(document.getElementById("ratingCount").value)
+        }
+    }
 
     // TODO 12: make a PUT request to the backend API and send the updatedProduct in the body as JSON
     try {
-
+        await fetch(`${PRODUCTS_API_URL}/${productId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedProduct)
+        })
+        alert(`Product with ${productId} updated successfully`)
+        loadProductDetailsPage(productId)
     } catch (error) {
-
+        alert("Error updating product in API")
     }
 }
 
